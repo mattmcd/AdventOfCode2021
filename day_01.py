ecc96b00443177d4eac29fb95b5ce8e5581fcd20
mattmcd <matt@matt-mcdonnell.com>
2021-12-01 06:40:57 +0000

    Advent of Code day 01: numpy solution

---
+++ day_01.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+x_test = np.array([199, 200, 208,210, 200, 207, 240, 269, 260, 263], dtype='int')
+
+
+def read_input():
+    with open('day_01_input.txt', 'r') as f:
+        return np.array(f.read().split('\n'), dtype='int')
+
+
+def part_01(x):
+    # Number of increases
+    return np.sum((x[1:] - x[0:-1]) > 0)
+
+
+def part_02(x):
+    # Number of increases of 3 element rolling window
+    y = np.cumsum(x)
+    return part_01(y[3:] - y[0:-3]) + 1
