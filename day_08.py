7d346e6249395f98c12eb74fb1f82fa3ad9ef5cd
mattmcd <matt@matt-mcdonnell.com>
2021-12-08 09:35:30 +0000

    Day 8 - better signature based approach

--- day_08.py
+++ day_08.py
@@ -38,23 +38,20 @@ def sort_str(s):
 def map_symbols(examples):
     digit_len_map = {2: 1, 3: 7, 4: 4, 7: 8}
     mapping = dict([(digit_len_map[len(s)], sort_str(s)) for s in examples if len(s) in digit_len_map])
-    seg_A = sort_str(set(mapping[7]) - set(mapping[1]))
-    mapping[3] = [sort_str(s) for s in examples if len(set(s) - set(mapping[1])) == 3 and len(s) == 5][0]
-    seg_G = sort_str((set(mapping[3]) - set(mapping[4])) - set(seg_A))
-    mapping[9] = sort_str(mapping[4] + seg_A + seg_G)
-    seg_E = sort_str(set(mapping[8]) - set(mapping[9]))
-    mapping[2] = [
-        sort_str(s) for s in examples
-        if len(s) == 5 and sort_str(s) not in mapping.values() and seg_E in s][0]
-    mapping[5] = [
-        sort_str(s) for s in examples
-        if len(s) == 5 and sort_str(s) not in mapping.values()][0]
-    seg_F = sort_str(set(mapping[1]) - set(mapping[5]))
-    mapping[6] = sort_str(mapping[5] + seg_E)
-    mapping[0] = [
-        sort_str(s) for s in examples
-        if len(s) == 6 and sort_str(s) not in mapping.values()][0]
-    segs = {'A': seg_A, 'E': seg_E, 'G': seg_G, 'F': seg_F}
+
+    def calc_mask(s, k):
+        return str(len(set(s) & set(mapping[k])))
+
+    def calc_sig(s):
+        return calc_mask(s, 1) + calc_mask(s, 4) + calc_mask(s, 7) + calc_mask(s, 8)
+
+    # Mapping of mask values to digit - determined using an earlier solution
+    sig_map = {'1325': 5, '1225': 2, '2335': 3, '2233': 7, '2436': 9, '1326': 6, '2336': 0, '2222': 1}
+
+    for e in examples:
+        if len(e) not in digit_len_map.values():
+            digit = sig_map[calc_sig(e)]
+            mapping[digit] = sort_str(e)
     str_to_digit = {v: k for k, v in mapping.items()}
     return str_to_digit
 
